8a642fadf89f520ef71f0170ad903f7861f16890
Martin Sloan <martinsloan@Martins-iMac.local>
2015-12-02 16:39:55 -0500

    adjust create views

--- resources/views/cluster/create.blade.php
+++ resources/views/cluster/create.blade.php
@@ -1,18 +1,25 @@
 @extends('app')
 
 @section('content')
-    <div class="container-fluid">
+
+    <div class="container-fluid table-top">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">New Cluster Settings</div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">New Cluster Settings</h3>
+                    </div>
                     <div class="panel-body">
+
                         {!! Form::open(['route' => 'cluster.store']) !!}
+
                             @include('cluster.partials.form', ['active' => false, 'version' => '10.5', 'userType' => 'application'])
+
                         {!! Form::close() !!}
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,22 +1,24 @@
 @extends('app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">New User Settings</div>
-                    <div class="panel-body">
 
-                        {!! Form::open(['route' => 'user.store']) !!}
+<div class="container-fluid table-top">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3>New User Settings</h3>
+                </div>
+                <div class="panel-body">
+                    {!! Form::open(['route' => 'user.store']) !!}
 
-                        @include('user.partials.form')
+                    @include('user.partials.form')
 
-                        {!! Form::close() !!}
-
-                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
     </div>
+</div>
+
 @endsection
