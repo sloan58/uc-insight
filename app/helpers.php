b54a1be1a580bb0a8db6c1c713c6ef0546957e2b
Martin Sloan <martinsloan@Martins-iMac.local>
2015-12-29 14:25:38 -0500

    add 8831 support for ITL

--- app/helpers.php
+++ app/helpers.php
@@ -153,9 +153,9 @@ function setKeys($model,$tleType)
             switch ($model) {
 
                 case "Cisco 7905":
+                case "Cisco 7906":
                 case "Cisco 7911":
                     return [
-
                         'Init:Applications',
                         'Key:Applications',
                         'Key:KeyPad3',
@@ -172,7 +172,6 @@ function setKeys($model,$tleType)
                         'Key:Soft4',
                         'Key:Soft2',
                         'Init:Applications',
-
                     ];
                     break;
 
@@ -240,7 +239,6 @@ function setKeys($model,$tleType)
 
                 case "Cisco 8961":
                 case "Cisco 9951":
-                case "Cisco 7937":
                 case "Cisco 9971":
                     return [
 
@@ -254,6 +252,20 @@ function setKeys($model,$tleType)
                         'Key:KeyPad4',
                         'Key:KeyPad4',
                         'Key:Soft3',
+                    ];
+                    break;
+
+                case "Cisco 8831":  //8831's interface is lazy, so we need to pause between each key press.
+                    return [
+                        'Key:Soft3',
+                        'Key:Sleep',
+                        'Key:KeyPad4',
+                        'Key:Sleep',
+                        'Key:KeyPad4',
+                        'Key:Sleep',
+                        'Key:Soft4',
+                        'Key:Sleep',
+                        'Key:Soft2',
                     ];
                     break;
 
